16667eb8f456df37dccb2abe3fa375bd24cb22ba
https://akostandov@bitbucket.org/akostandov/edup.git <armankostandov@gmail.com>
2021-05-28 17:44:34 +0300

    initial commit

---
+++ src/main/java/ru/foreverjun/msscbreweryclient/MsscBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package ru.foreverjun.msscbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryClientApplication.class, args);
+	}
+
+}
